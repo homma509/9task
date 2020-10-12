e1d737b26c60ae4e3ae69ed008d955257fe70f66
homma509 <kouki.honma@gmail.com>
2020-10-13 08:41:25 +0900

    add create task method to task service

---
+++ task/service.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes"
+	pbActivity "github.com/homma509/9task/proto/activity"
+	pbProject "github.com/homma509/9task/proto/project"
+	pbTask "github.com/homma509/9task/proto/task"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// TaskService Taskサービス構造体
+type TaskService struct {
+	store          Store
+	activityClient pbActivity.ActivityServiceClient
+	projectClient  pbProject.ProjectServiceClient
+}
+
+// CreateTask Taskの登録
+func (s *TaskService) CreateTask(
+	ctx context.Context,
+	req *pbTask.CreateTaskRequest,
+) (*pbTask.CreateTaskResponse, error) {
+	if req.GetName() == "" {
+		return nil, status.Error(codes.InvalidArgument,
+			"empty task name")
+	}
+
+	resp, err := s.projectClient.FindProject(ctx,
+		&pbProject.FindProjectRequest{ProjectId: req.GetProjectId()})
+	if err != nil {
+		return nil, status.Error(
+			codes.NotFound, err.Error())
+	}
+	//TODO dummyID
+	userID := uint64(1)
+	now := ptypes.TimestampNow()
+	task, err := s.store.CreateTask(&pbTask.Task{
+		Name:      req.GetName(),
+		Status:    pbTask.Status_WAITING,
+		UserId:    userID,
+		ProjectId: resp.Project.GetId(),
+		CreatedAt: now,
+		UpdatedAt: now,
+	})
+	if err != nil {
+		return nil, status.Error(
+			codes.InvalidArgument, err.Error(),
+		)
+	}
+
+	content := &pbActivity.CreateTaskContent{
+		TaskId:   task.GetId(),
+		TaskName: task.GetName(),
+	}
+	any, err := ptypes.MarshalAny(content)
+	if err != nil {
+		return nil, status.Error(
+			codes.InvalidArgument, err.Error(),
+		)
+	}
+	if _, err := s.activityClient.CreateActivity(ctx,
+		&pbActivity.CreateActivityRequest{
+			Content: any,
+		}); err != nil {
+		return nil, err
+	}
+	return &pbTask.CreateTaskResponse{Task: task}, nil
+}
