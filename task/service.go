c804b6172ea21f77ee6454fee819a27cfcb8dbbe
homma509 <kouki.honma@gmail.com>
2020-10-14 19:33:42 +0900

    add find tasks func

--- task/service.go
+++ task/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/empty"
 	pbActivity "github.com/homma509/9task/proto/activity"
 	pbProject "github.com/homma509/9task/proto/project"
 	pbTask "github.com/homma509/9task/proto/task"
@@ -34,7 +35,7 @@ func (s *TaskService) CreateTask(
 		return nil, status.Error(
 			codes.NotFound, err.Error())
 	}
-	//TODO dummyID
+	// TODO Placeholder ID
 	userID := uint64(1)
 	now := ptypes.TimestampNow()
 	task, err := s.store.CreateTask(&pbTask.Task{
@@ -68,4 +69,19 @@ func (s *TaskService) CreateTask(
 		return nil, err
 	}
 	return &pbTask.CreateTaskResponse{Task: task}, nil
+}
+
+// FindTasks ユーザーに関連するタスクの検索
+func (s *TaskService) FindTasks(
+	ctx context.Context,
+	_ *empty.Empty,
+) (*pbTask.FindTasksResponse, error) {
+	// TODO Placeholder ID
+	userID := uint64(1)
+	tasks, err := s.store.FindTasks(userID)
+	if err != nil {
+		return nil, status.Error(
+			codes.InvalidArgument, err.Error())
+	}
+	return &pbTask.FindTasksResponse{Tasks: tasks}, nil
 }
