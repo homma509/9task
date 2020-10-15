6f8d9a393dce1fd5f5346ef5345cdf3517e11a42
homma509 <kouki.honma@gmail.com>
2020-10-16 00:00:56 +0900

    add Logging func

--- shared/interceptor/interceptor.go
+++ shared/interceptor/interceptor.go
@@ -2,19 +2,46 @@ package interceptor
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/homma509/9task/shared/md"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
+// XTraceID コンテキストにTraceIDの追加
 func XTraceID() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context,
-			req interface{},
-			info *grpc.UnaryServerInfo,
-			handler grpc.UnaryHandler) (interface{}, error) {
-				traceID := md.GetTraceIDFromContext(ctx)
-				ctx := md.AddTraceIDToContext(ctx, traceID)
-				return handler(ctx, req)
-			}
-	)
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler) (interface{}, error) {
+		traceID := md.GetTraceIDFromContext(ctx)
+		ctx = md.AddTraceIDToContext(ctx, traceID)
+		return handler(ctx, req)
+	}
+}
+
+const loggingFmt = "TraceID:%s\tFullMethod:%s\tElapsedTime:%s\tStatusCode:%s\tError:%s\n"
+
+// Logging ログの出力
+func Logging() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		start := time.Now()
+		h, err := handler(ctx, req)
+		var errMsg string
+		if err != nil {
+			errMsg = err.Error()
+		}
+		log.Printf(loggingFmt,
+			md.GetTraceIDFromContext(ctx),
+			info.FullMethod,
+			time.Since(start),
+			status.Code(err), errMsg)
+		return h, err
+	}
 }
