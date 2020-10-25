3d5b152a5037edda6a759eabc70ecc9252f90b64
homma509 <kouki.honma@gmail.com>
2020-10-25 12:12:10 +0900

    add struct and constructor for task

--- task/store.go
+++ task/store.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	pbTask "github.com/homma509/9task/proto/task"
+	"github.com/homma509/9task/shared/inmemory"
 )
 
 // Store Task Repository Interface
@@ -11,4 +12,14 @@ type Store interface {
 	FindTasks(userID uint64) ([]*pbTask.Task, error)
 	FindProjectTasks(projectID, userID uint64) ([]*pbTask.Task, error)
 	updateTask(task *pbTask.Task) (*pbTask.Task, error)
+}
+
+// StoreOnMemory タスクのMap構造体
+type StoreOnMemory struct {
+	tasks *inmemory.IndexMap
+}
+
+// NewStoreOnMemory タスクのMap構造体の生成
+func NewStoreOnMemory() *StoreOnMemory {
+	return &StoreOnMemory{inmemory.NewIndexMap()}
 }
